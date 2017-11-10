dd46bfd40e62ed36b949f985736bdcf08de87626
jimbo <jimbolix@163.com>
2017-11-10 14:56:36 +0800

    aop配置

---
+++ src/main/java/init/AopConfig.java
@@ -0,0 +1,22 @@
+package init;
+
+import aop.AopCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author ruihui.li
+ * @version V1.0
+ * @Description: ${todo}(用一句话描述该文件做什么)
+ * @date 2017/11/6
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+
+    @Bean
+    public AopCase aopCase(){
+        return new AopCase();
+    }
+}
