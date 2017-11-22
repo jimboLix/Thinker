5c70f2317b2fefb70515377251ad94ebaabb834e
jimbo <jimbolix@163.com>
2017-11-22 19:49:17 +0800

    常量工具类

---
+++ src/main/java/util/ConstansUtil.java
@@ -0,0 +1,17 @@
+package util;
+
+/**
+ * @author ruihui.li
+ * @version V1.0
+ * @Description: 常量
+ * @date 2017/11/21
+ */
+public class ConstansUtil {
+
+    public static final String INTERCEPT_URI = ".do";
+
+    public static final String USER_ON_SESSION_NAME = "user";
+
+    public static final String LOGINURL = "/home/login.do";
+    public static final String DOLOGINURL = "/home/doLogin.do";
+}
